b10f54cabc587b72a78ae922c82503538c96d5b1
Tnatan <natanmoraes5388@gmail.com>
2023-11-01 18:25:23 -0300

    Initial version of web exo api

---
+++ Controllers/ProjetosController.cs
@@ -0,0 +1,23 @@
+
+using Exo.WebApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjetosController : ControllerBase
+    {
+        private readonly ProjetoRepository _projetoRepository;
+        public ProjetosController(ProjetoRepository projetoRepository)
+        {
+            _projetoRepository = projetoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(_projetoRepository.Listar());
+        }
+    }    
+}
